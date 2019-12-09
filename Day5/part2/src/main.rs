ade7c2a81fade5c6343095949d80d56fddf3a0ae
jonathan-haubrich <spacecowboy4@gmail.com>
2019-12-09 13:51:06 -0500

    Day5 Part2. Pretty disgusting. Will have to encapsulate logic in a class for future

---
+++ Day5/part2/src/main.rs
@@ -0,0 +1,171 @@
+
+use std::env;
+use std::fs;
+use std::process;
+
+enum Modes {
+    Position = 0,
+    Immediate = 1,
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 3 {
+        eprintln!("Usage: {} <input> <input(s)>", args[0]);
+        process::exit(1);
+    }
+
+    let input = args[2].parse().unwrap();
+    println!("input: {}", input);
+
+    let mem: Vec<i32> = fs::read_to_string(&args[1]).unwrap()
+        .trim()
+        .split(',')
+        .map(|s| { s.trim().parse().unwrap_or_else(|err| {
+            eprintln!("Failed to unwrap: {}", s);
+            process::exit(1);
+        })})
+        .collect();
+
+    let mut opcodes = mem.to_vec();
+    let mut pc = 0;
+    loop {
+        let mut opcode: usize = opcodes[pc] as usize;
+        let mut instruction = opcode % 100;
+        opcode /= 100;
+        let mut param1_mode = opcode % 10;
+        opcode /= 10;
+        let mut param2_mode = opcode % 10;
+        opcode /= 10;
+        let mut param3_mode = opcode % 10;
+
+        let mut param1;
+        let mut param2;
+        let mut destination;
+        match instruction {
+            1 => {
+                param1 = opcodes[pc + 1];
+                if param1_mode == Modes::Position as usize {
+                    param1 = opcodes[param1 as usize];
+                }
+
+                param2 = opcodes[pc + 2];
+                if param2_mode == Modes::Position as usize {
+                    param2 = opcodes[param2 as usize];
+                }
+
+                destination = opcodes[pc + 3]; 
+
+                opcodes[destination as usize] = param1 + param2;
+                pc += 4;
+            },
+            2 => {
+                param1 = opcodes[pc + 1];
+                if param1_mode == Modes::Position as usize {
+                    param1 = opcodes[param1 as usize];
+                }
+
+                param2 = opcodes[pc + 2];
+                if param2_mode == Modes::Position as usize {
+                    param2 = opcodes[param2 as usize];
+                }
+
+                destination = opcodes[pc + 3];
+
+                opcodes[destination as usize] = param1 * param2;
+                pc += 4;
+            },
+            3 => {
+                destination = opcodes[pc + 1];
+                opcodes[destination as usize] = input;
+                pc += 2;
+            },
+            4 => {
+                destination = opcodes[pc + 1];
+                if param1_mode == Modes::Position as usize {
+                    destination = opcodes[destination as usize];
+                }
+
+                println!("{}", destination);
+                pc += 2;
+            },
+            5 => {
+                param1 = opcodes[pc + 1];
+                if param1_mode == Modes::Position as usize {
+                    param1 = opcodes[param1 as usize];
+                }
+
+                param2 = opcodes[pc + 2];
+                if param2_mode == Modes::Position as usize {
+                    param2 = opcodes[param2 as usize];
+                }
+
+                if param1 != 0 {
+                    pc = param2 as usize;
+                } else {
+                    pc += 3;
+                }
+            },
+            6 => {
+                param1 = opcodes[pc + 1];
+                if param1_mode == Modes::Position as usize {
+                    param1 = opcodes[param1 as usize];
+                }
+
+                param2 = opcodes[pc + 2];
+                if param2_mode == Modes::Position as usize {
+                    param2 = opcodes[param2 as usize];
+                }
+
+                if param1 == 0 {
+                    pc = param2 as usize;
+                } else {
+                    pc += 3;
+                }
+            },
+            7 => {
+                param1 = opcodes[pc + 1];
+                if param1_mode == Modes::Position as usize {
+                    param1 = opcodes[param1 as usize];
+                }
+
+                param2 = opcodes[pc + 2];
+                if param2_mode == Modes::Position as usize {
+                    param2 = opcodes[param2 as usize];
+                }
+
+                destination = opcodes[pc + 3];
+
+                if param1 < param2 {
+                    opcodes[destination as usize] = 1;
+                } else {
+                    opcodes[destination as usize] = 0;
+                }
+                pc += 4;
+            },
+            8 => {
+                param1 = opcodes[pc + 1];
+                if param1_mode == Modes::Position as usize {
+                    param1 = opcodes[param1 as usize];
+                }
+
+                param2 = opcodes[pc + 2];
+                if param2_mode == Modes::Position as usize {
+                    param2 = opcodes[param2 as usize];
+                }
+
+                destination = opcodes[pc + 3];
+
+                if param1 == param2 {
+                    opcodes[destination as usize] = 1;
+                } else {
+                    opcodes[destination as usize] = 0;
+                }
+                pc += 4;
+            },
+            99 => break,
+            _ => panic!("Invalid opcode: ({}) {}", pc, opcodes[pc])
+        }
+    }
+}
